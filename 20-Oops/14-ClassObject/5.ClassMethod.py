4dfc904b0972daee36f32f3e2bef2b5a4ec5869e
Kilaripadmavathikumari <kpadmavathikumari@gamil.com>
2026-02-03 20:04:51 +0530

    Python

---
+++ 20-Oops/14-ClassObject/5.ClassMethod.py
@@ -0,0 +1,26 @@
+class College:
+    # Class variable
+    college_name = "ABC College"
+
+    # Class method
+    @classmethod
+    def show_college(cls):
+        print("College Name:", cls.college_name)
+
+# Calling class method using class name
+College.show_college()
+
+'''class Rectangle:
+    count=0
+    def __init__(self,l,b):
+        self.length=l
+        self.breadth=b
+        Rectangle.count+=1
+
+    @classmethod
+    def get_Count(cls):
+        return cls.count
+r1=Rectangle(15,8)
+r2=Rectangle(7,4)
+print(Rectangle.get_Count())'''
+        
